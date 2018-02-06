f4f10abdcfad10a6a3cd08cf28b198a51be462c1
Alex Ballera <alexballera@gmail.com>
2018-02-06 16:36:49 -0400

    Commit inicial

---
+++ src/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function highend_child_theme_enqueue_styles() {
+
+    $parent_style = 'highend-parent-style';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+
+    wp_enqueue_style( 'highend-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style )
+    );
+}
+add_action( 'wp_enqueue_scripts', 'highend_child_theme_enqueue_styles' );
+
+?>
---
+++ tasks/images.js
@@ -0,0 +1,50 @@
+import gulp from 'gulp'
+import cache from 'gulp-cache'
+import imagemin from 'gulp-imagemin'
+import pngquant from 'imagemin-pngquant'
+import imageminSvgo from 'imagemin-svgo'
+import imageminOptipng from 'imagemin-optipng'
+import imageminJpegtran from 'imagemin-jpegtran'
+
+gulp.task('images', ['screenshot'], () => {
+  return gulp.src('./src/images/**')
+    .pipe(cache(imagemin({
+      optimizationLevel: 7,
+      progressive: true,
+      interlaced: true,
+      multipass: true,
+      use: [
+        pngquant(),
+        imageminSvgo(),
+        imageminOptipng({optimizationLevel: 7}),
+        imageminJpegtran({progressive: true})
+      ],
+      svgoPlugins: [
+        { removeViewBox: false }, // don't remove the viewbox atribute from the SVG
+        { removeUselessStrokeAndFill: false }, // don't remove Useless Strokes and Fills
+        { removeEmptyAttrs: false } // don't remove Empty Attributes from the SVG
+      ]
+    })))
+    .pipe(gulp.dest('./public/images'))
+})
+gulp.task('screenshot', () => {
+  gulp.src('./src/*.png')
+    .pipe(cache(imagemin({
+      optimizationLevel: 7,
+      progressive: true,
+      interlaced: true,
+      multipass: true,
+      use: [
+        pngquant(),
+        imageminSvgo(),
+        imageminOptipng({optimizationLevel: 7}),
+        imageminJpegtran({progressive: true})
+      ],
+      svgoPlugins: [
+        { removeViewBox: false }, // don't remove the viewbox atribute from the SVG
+        { removeUselessStrokeAndFill: false }, // don't remove Useless Strokes and Fills
+        { removeEmptyAttrs: false } // don't remove Empty Attributes from the SVG
+      ]
+    })))
+    .pipe(gulp.dest('./public'))
+})
